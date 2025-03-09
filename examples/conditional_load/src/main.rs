9c3fb9b23e40b8247b56b5419d3caba751ddfc00
Sondre <sbrynjulf@hotmail.com>
2025-03-09 15:46:59 +0100

    feat: add support for unit enums

--- examples/conditional_load/src/main.rs
+++ examples/conditional_load/src/main.rs
@@ -7,37 +7,35 @@
 #![allow(dead_code)]
 use envoke::{Envoke, Fill};
 
+#[derive(Debug, Fill, strum::EnumString)]
+#[strum(serialize_all = "UPPERCASE")]
+#[fill(rename_all = "SCREAMING_SNAKE_CASE")]
+enum LogLevel {
+    Debug,
+    Info,
+    #[fill(default)]
+    Error,
+}
+
 #[derive(Debug, Fill)]
-#[fill(
-    rename_all = "SCREAMING_SNAKE_CASE",
-    prefix = "PRODUCTION",
-    delimiter = "_"
-)]
+#[fill(rename_all = "SCREAMING_SNAKE_CASE")]
 struct Production {
-    #[fill(env)]
-    api_port: u16,
+    #[fill(env, default = LogLevel::Error)]
+    log_level: LogLevel,
 }
 
 #[derive(Debug, Fill)]
-#[fill(
-    rename_all = "SCREAMING_SNAKE_CASE",
-    prefix = "STAGING",
-    delimiter = "_"
-)]
+#[fill(rename_all = "SCREAMING_SNAKE_CASE")]
 struct Staging {
-    #[fill(env)]
-    api_port: u16,
+    #[fill(env, default = LogLevel::Info)]
+    log_level: LogLevel,
 }
 
 #[derive(Debug, Fill)]
-#[fill(
-    rename_all = "SCREAMING_SNAKE_CASE",
-    prefix = "DEVELOPMENT",
-    delimiter = "_"
-)]
+#[fill(rename_all = "SCREAMING_SNAKE_CASE")]
 struct Development {
-    #[fill(env)]
-    api_port: u16,
+    #[fill(env, default = LogLevel::Debug)]
+    log_level: LogLevel,
 }
 
 #[derive(Debug, Fill)]
@@ -55,20 +53,15 @@ pub struct Environment {
 }
 
 fn main() {
-    temp_env::with_vars(
-        [
-            ("ENVIRONMENT", Some("PRODUCTION")),
-            ("PRODUCTION_API_PORT", Some("8000")),
-            ("STAGING_API_PORT", Some("8001")),
-            ("DEVELOPMENT_API_PORT", Some("8002")),
-        ],
-        || {
-            // or use `try_envoke()` for fail nice variant
-            let mode = Mode::envoke();
-            println!("{mode:#?}");
+    temp_env::with_vars([("ENVIRONMENT", Some("PRODUCTION"))], || {
+        // or use `try_envoke()` for fail nice variant
+        let mode = Mode::envoke();
+        println!("{mode:?}");
 
-            let env = Environment::envoke();
-            println!("{env:#?}");
-        },
-    )
+        let log_level = LogLevel::envoke();
+        println!("{log_level:?}");
+
+        let env = Environment::envoke();
+        println!("{env:#?}");
+    })
 }
--- modules/envoke_derive/src/derive/structs/mod.rs
+++ modules/envoke_derive/src/derive/structs/mod.rs
@@ -2,14 +2,15 @@ use attrs::{ContainerAttributes, FieldAttributes};
 use proc_macro2::{Span, TokenStream};
 use quote::quote;
 use syn::{spanned::Spanned, Data, DeriveInput, Fields, FieldsNamed, Ident, Type};
-use utils::{generate_default_call, generate_env_call};
+use utils::generate_field_calls;
 
 use crate::errors::Error;
 
 mod attrs;
 mod utils;
 
-struct Field {
+#[derive(Debug)]
+pub struct Field {
     ident: Option<Ident>,
     ty: Type,
     attrs: FieldAttributes,
@@ -45,34 +46,13 @@ pub fn derive_for(input: DeriveInput) -> syn::Result<TokenStream> {
 
     let struct_name = &input.ident;
     let struct_data = get_struct_data(input.span(), input.data)?;
+    let fields: Vec<Field> = struct_data
+        .named
+        .into_iter()
+        .map(Field::try_from)
+        .collect::<syn::Result<_>>()?;
 
-    let mut field_assignments = Vec::new();
-    for field in struct_data.named {
-        // Extract only what we need
-        let field = Field::try_from(field)?;
-
-        let ident = &field.ident;
-        let ty = &field.ty;
-
-        let value_call = if field.attrs.is_nested {
-            quote! {
-                <#ty as envoke::Envoke>::try_envoke()?
-            }
-        } else if let Some(envs) = &field.attrs.envs {
-            generate_env_call(&envs, &c_attrs, &field)
-        } else if let Some(default) = &field.attrs.default {
-            generate_default_call(&default, &field)
-        } else {
-            // Caught by another check
-            unreachable!()
-        };
-
-        let call = quote! {
-            #ident: #value_call
-        };
-
-        field_assignments.push(call);
-    }
+    let field_calls = generate_field_calls(c_attrs, fields)?;
 
     let expanded = quote! {
         impl #impl_generics envoke::Envoke for #struct_name #type_generics #where_clause {
@@ -80,7 +60,7 @@ pub fn derive_for(input: DeriveInput) -> syn::Result<TokenStream> {
                 use envoke::{Envloader, OptEnvloader, FromMap, FromMapOpt, FromSet, FromSetOpt};
 
                 Ok(#struct_name {
-                    #(#field_assignments),*
+                    #(#field_calls),*
                 })
             }
         }
